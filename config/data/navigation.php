a6659073de513e3f646ec9300bf409a2919dc2a1
PietroDanieli <1pie7d@gmail.com>
2022-11-02 13:54:54 +0100

    import navigation.php

---
+++ config/data/navigation.php
@@ -0,0 +1,81 @@
+<?php
+ return [
+    "header-nav"=>[
+        [
+        "label"=> "characters",
+        "active"=> false,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "comics",
+        "active"=> true,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "movies",
+        "active"=> false,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "tv",
+        "active"=> false,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "games",
+        "active"=> false,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "collectibles",
+        "active"=> true,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "videos",
+        "active"=> false,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "fans",
+        "active"=> false,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "news",
+        "active"=> false,
+        "href"=> "#",
+      ],
+      [
+        "label"=> "shop",
+        "active"=> false,
+        "href"=> "#",
+      ],
+
+    ],
+    "shop-nav"=>[
+        [
+          "url"=> "/public/img/buy-comics-digital-comics.png",
+          "description"=> "Digital Comics",
+        ],
+        [
+          "url"=> "/public/img/buy-comics-merchandise.png",
+
+          "description"=> "DC Merchandise",
+        ],
+        [
+          "url"=> "/public/img/buy-comics-subscriptions.png",
+
+          "description"=> "subscription",
+        ],
+        [
+          "url"=> "/public/img/buy-comics-shop-locator.png",
+          "description"=> "comic shop locator ",
+        ],
+        [
+          "url"=> "/public/img/buy-dc-power-visa.svg",
+          "description"=> "dc power visa",
+        ],
+    ],
+
+];
